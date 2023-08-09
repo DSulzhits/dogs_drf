24b4580fe72855bf3034f66aab8711e9fac7e2c3
DSulzhits <dsulzhits@gmail.com>
2023-08-09 14:48:23 +0300

    add app-dogs urlpatterns

---
+++ dogs/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from rest_framework import routers
+
+from dogs.views.dog import *
+from dogs.views.breed import *
+
+from dogs.apps import DogsConfig
+
+app_name = DogsConfig.name
+
+urlpatterns = [
+    path('', DogListView.as_view()),
+    path('<int:pk>/', DogDetailView.as_view()),
+    path('create/', DogCreateView.as_view()),
+    path('<int:pk>/update/', DogUpdateView.as_view()),
+    path('<int:pk>/delete/', DogDeleteView.as_view()),
+]
+
+router = routers.SimpleRouter()
+router.register('breed', BreedViewSet)
+
+urlpatterns += router.urls
