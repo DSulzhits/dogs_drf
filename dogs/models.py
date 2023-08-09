8fb0f297cabd327db6a6d9474cbbb7aff18c1503
DSulzhits <dsulzhits@gmail.com>
2023-08-09 14:47:32 +0300

    add models: Dog Breed

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Кличка")
+    breed = models.ForeignKey("dogs.Breed", verbose_name="Порода собаки", related_name="dogs",
+                              on_delete=models.SET_NULL, null=True)
+    photo = models.ImageField(upload_to="dog_photo", verbose_name="Картинка", **NULLABLE)
+    date_born = models.DateField(verbose_name="Дата рождения", null=True)
+    # owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Собака"
+        verbose_name_plural = "Собаки"
+        ordering = ['breed', 'name', ]
+
+    def __str__(self):
+        return self.name
+
+
+class Breed(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "Порода"
+        verbose_name_plural = "Породы"
+        ordering = ['name', ]
+
+    def __str__(self):
+        return self.name
