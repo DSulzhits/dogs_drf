28f5cceebb1252466c22967fd8ccc924b6ae55cf
DSulzhits <dsulzhits@gmail.com>
2023-08-09 14:47:46 +0300

    add model: User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from dogs.models import NULLABLE
 
-# Create your models here.
+
+class User(AbstractUser):
+    age = models.PositiveIntegerField(**NULLABLE)
