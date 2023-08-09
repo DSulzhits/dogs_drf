3c919c3e73eb26cdc11f7b7f25f8f0c642a2d39f
DSulzhits <dsulzhits@gmail.com>
2023-08-09 14:58:48 +0300

    Create model UserRoles, update model User

--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from dogs.models import NULLABLE
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
 
 
 class User(AbstractUser):
     age = models.PositiveIntegerField(**NULLABLE)
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
