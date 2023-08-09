757f3f6c4811cc2153f94b315e74c3e7067a65f9
DSulzhits <dsulzhits@gmail.com>
2023-08-09 14:48:58 +0300

    update base settings

--- dogs_drf/settings.py
+++ dogs_drf/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
+
+    'dogs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +84,9 @@ postgres_key = os.getenv('POSTGRESSQL_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shelter',
+        'NAME': 'dogs_drf',
         'USER': 'postgres',
+        'PORT': '5432',
         'PASSWORD': postgres_key
     }
 }
@@ -124,3 +129,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4,
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication', ],
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', ],
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ],
+}
