4d5ea0372faf19ac89e39d8f6ac71fc4606e24e0
DSulzhits <dsulzhits@gmail.com>
2023-08-18 09:46:41 +0300

    add: test_get_list test_dog_create test_dog_create_validation_error

--- dogs/tests.py
+++ dogs/tests.py
@@ -1,3 +1,106 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from dogs.models import Breed, Dog
+from users.models import User, UserRoles
+
+
+class DogTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        # self.user = User.objects.create(
+        #     username='tester',
+        #     is_staff=True,
+        #     is_superuser=True,
+        #     is_active=True,
+        #     role=UserRoles.MODERATOR
+        # )
+        # self.user.set_password('qwerty')
+        # self.user.save()
+        # response = self.client.post()
+
+        self.breed = Breed.objects.create(
+            name='test',
+            description='testing_breed'
+        )
+
+        self.dog = Dog.objects.create(
+            name='test',
+            breed=self.breed
+        )
+
+    def test_get_list(self):
+        """Test for getting list of dogs"""
+
+        response = self.client.get(
+            reverse('dogs:dog_list')
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {
+                "count": 1,
+                "next": None,
+                "previous": None,
+                "results": [
+                    {
+                        "name": self.dog.name,
+                        "breed": self.dog.breed.name
+                    }
+                ]
+            }
+        )
+
+    def test_dog_create(self):
+        """Test dog creating"""
+        data = {
+            'name': 'test2',
+            'category': self.breed.id
+        }
+
+        response = self.client.post(
+            reverse('dogs:dog_create'),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            Dog.objects.all().count(),
+            2
+        )
+
+    def test_dog_create_validation_error(self):
+        """Test validation error"""
+        data = {
+            'name': 'крипта',
+            'category': self.breed.id
+        }
+
+        response = self.client.post(
+            reverse('dogs:dog_create'),
+            data=data
+        )
+
+        # print(response.json())
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+
+        self.assertEqual(
+            response.json(),
+            {'name': ['Использованы запрещенные слова!']}
+        )
