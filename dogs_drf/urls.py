021e8bf12df4a3259275e885107006dc0149f3a9
DSulzhits <dsulzhits@gmail.com>
2023-08-23 10:55:50 +0300

    update urls

--- dogs_drf/urls.py
+++ dogs_drf/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
     path('dog/', include("dogs.urls", namespace='dogs')),
     path('user/', include("users.urls", namespace='users')),
 
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
